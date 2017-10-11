84c35a3c11acad3fb4e8ab980e23b12b04f52e1d
Isashi <isashimitsui@gmail.com>
2017-10-11 23:12:19 +0200

    fixed view with less than 12pics

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,11 +1,16 @@
 module FlickrHelper
-  def user_photos(user_id, photo_count = 12)
+  def user_photos(user_id, photo_count)
     flickr.photos.search(:user_id => user_id).first(photo_count)
   end
 
-  def render_flickr_sidebar_widget(user_id, photo_count = 12, columns = 2)
+  def user_photo_count(user_id)
+    flickr.photos.search(:user_id => user_id).count
+  end
+
+  def render_flickr_sidebar_widget(user_id, columns = 2)
     begin
-      photos = user_photos(user_id, photo_count).in_groups_of(2)
+      photo_count = [user_photo_count(user_id),12].min
+      photos = user_photos(user_id, photo_count).in_groups_of(3)
       render  :partial => '/flickr/sidebar_widget', 
               :locals => { :photos => photos }
     rescue => exception

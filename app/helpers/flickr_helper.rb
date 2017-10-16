92d41cb139b5166f9fb8e45eb4dacb323e1d139c
Andy Duss <mindovermiles262@gmail.com>
2017-10-15 18:28:00 -0700

    wip

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -7,6 +7,17 @@ module FlickrHelper
     flickr.photos.search(:text => user_id, :license => license, :sort => sort, :safe_search => safety).count
   end
 
+=begin
+  *** VALID SAFE-SEARCH API CALL from web API ***
+  method=flickr.photos.search
+  &api_key=4129408b320ee232c70d66a0c89c08aa
+  &safe_search=3
+  &format=json
+  &nojsoncallback=1
+  &auth_token=72157687120645091-eb2e82ddad745771
+  &api_sig=3edc9c86edb5dff1026d003f03886c75
+=end
+
   def render_flickr_sidebar_widget(user_id, license, sort, safety, columns = 2)
     begin
       photo_count = [user_photo_count(user_id, license, sort, safety),100].min
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 	root "static_pages#index"
+	get '/authorize', to: "static_pages#authorize"
+	get '/callback', to: "static_pages#callback"
 end

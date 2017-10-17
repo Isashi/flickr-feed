a43f157c2c769cde61241207fa03b51afb5f1a57
Andy Duss <mindovermiles262@gmail.com>
2017-10-17 08:36:29 -0700

    cleanup code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,12 @@
 class SessionsController < ApplicationController
-  require 'yaml'
   def authorize
 		flickr = FlickRaw::Flickr.new
 		token = flickr.get_request_token(:oauth_callback => 'http://localhost:3000/callback')
-		# You'll need to store the token somewhere for when the user is returned to the callback method
-		# I stick mine in memcache with their session key as the cache key
 		session[:token] = token
-
 		@auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'read')
-		# Stick @auth_url in your template for users to click
-		
 	end
 
   def callback
-    require 'yaml'
-    require 'objspace'
 		user = FlickRaw::Flickr.new
 		request_token = session[:token]
 		oauth_token = params[:oauth_token]

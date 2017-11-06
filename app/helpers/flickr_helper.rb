c2d181d14614ec481127e09385fe62606e342e1c
Isashi Mitsui <isashimitsui@gmail.com>
2017-11-06 23:26:24 +0000

    add :per_page and :page to flickr_helper

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,15 +1,15 @@
 module FlickrHelper
   def user_photos(user_id, license, sort, safe_search, photo_count)
-    flickr.photos.search(:text => user_id, :license => license, :sort => sort, :safe_search => safe_search).first(photo_count)
+    flickr.photos.search(:text => user_id, :license => license, :sort => sort, :safe_search => safe_search, :per_page => 60, :page => 1).first(photo_count)
   end
 
   def user_photo_count(user_id, license, sort, safe_search)
-    flickr.photos.search(:text => user_id, :license => license, :sort => sort, :safe_search => safe_search).count
+    flickr.photos.search(:text => user_id, :license => license, :sort => sort, :safe_search => safe_search, :per_page => 60, :page => 1).count
   end
 
   def render_flickr_sidebar_widget(user_id, license, sort, safe_search, columns = 2)
     begin
-      photo_count = [user_photo_count(user_id, license, sort, safe_search),100].min
+      photo_count = [user_photo_count(user_id, license, sort, safe_search),500].min
       photos = user_photos(user_id, license, sort, safe_search, photo_count)
       render  :partial => '/flickr/sidebar_widget', 
               :locals => { :photos => photos }
